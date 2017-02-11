ec67215b38ea9c63e66ed784c74b5514985afb67
IslamWahid <eng.islamwahid@gmail.com>
2017-02-11 19:34:40 +0200

    class product tested and working well with DB still needs error handling

--- classes.php/product.php
+++ classes.php/product.php
@@ -8,24 +8,25 @@ class product{
     private $image;
     private $idcategory;
     private $isdeleted;
-//=======================================================================
-    function __construct($name,$price,$quantity,$description,$image,$idcategory){
-        $idproduct=null;
-        $this->$name=$name;
-        $this->$price=$price;
-        $this->$quantity=$quantity;
-        $this->$description=$description;
-        $this->$image=$image;
-        $this->$idcategory=$idcategory;
-        $this->$isdeleted=0;
-    }
+//=======================================================================================================
     function __get($attr){
         return $this->$attr;
     }
     function __set($attr,$value){
         $this->$attr = $value; //variable variable
     }
-
+    static function createobj($name,$price,$quantity,$description,$image,$idcategory){
+        $obj=new self();
+        $obj->idproduct=null;
+        $obj->name=$name;
+        $obj->price=$price;
+        $obj->quantity=$quantity;
+        $obj->description=$description;
+        $obj->image=$image;
+        $obj->idcategory=$idcategory;
+        $obj->isdeleted=0;
+        return $obj;
+    }
     static function selectAll() {
         require 'config.php';
         $stmt = $mysqli->prepare("SELECT * FROM `E-Commerce`.product");
@@ -36,10 +37,9 @@ class product{
         }
         return $products;
     }
-
     static function selectbyname($name) {
         require 'config.php';
-        $stmt = $mysqli->prepare("SELECT * FROM `E-Commerce`.product WHERE `name` = <?>");
+        $stmt = $mysqli->prepare("SELECT * FROM `E-Commerce`.product WHERE `name` = ?");
         $stmt->bind_param('s', $name);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -48,38 +48,34 @@ class product{
         }
         return $products;
     }
-
     static function selectbyid($idproduct) {
         require 'config.php';
-        $stmt = $mysqli->prepare("SELECT * FROM `E-Commerce`.product WHERE `idproduct` = <?>");
+        $stmt = $mysqli->prepare("SELECT * FROM `E-Commerce`.product WHERE `idproduct` = ?");
         $stmt->bind_param('i', $idproduct);
         $stmt->execute();
         $result = $stmt->get_result();
-        return $obj = $result->fetch_object('product');
+        return $result->fetch_object('product');
     }
-
     function insert() {
         require 'config.php';
         $stmt = $mysqli->prepare("INSERT INTO `E-Commerce`.`product` VALUES(null,?,?,?,?,?,?,?)");
         $stmt->bind_param('sdissii',$this->name,$this->price,$this->quantity,$this->description,$this->image,$this->idcategory,$this->isdeleted);
         return $stmt->execute();
     }
-
     function update() {
         require 'config.php';
         $stmt = $mysqli->prepare("UPDATE `E-Commerce`.`product`
             SET
-            `name` = <?>,
-            `price` = <?>,
-            `quantity` = <?>,
-            `description` = <?>,
-            `image` = <?>,
-            `idcategory` = <?>,
-            `isdeleted` = <?>
-            WHERE `idproduct` = <$this->idproduct>
+            `name` = ?,
+            `price` = ?,
+            `quantity` = ?,
+            `description` = ?,
+            `image` = ?,
+            `idcategory` = ?,
+            `isdeleted` = ?
+            WHERE `idproduct` = $this->idproduct
             ");
         $stmt->bind_param('sdissii',$this->name,$this->price,$this->quantity,$this->description,$this->image,$this->idcategory,$this->isdeleted);
         return $stmt->execute();
     }
-//=======================================================================
 }?>
