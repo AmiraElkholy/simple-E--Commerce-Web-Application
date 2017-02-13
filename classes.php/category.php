6c22d3c2df7e78c2930f4a3c9fc0d4752dbff80f
Sherifabdulmawla <sherifabdulmawla@gmail.com>
2017-02-13 16:20:29 +0200

    Adding Select Super Categories Method

--- classes.php/category.php
+++ classes.php/category.php
@@ -20,18 +20,7 @@ class category{
         $query="insert into category(name,idsupercategory) values(?,?)";
         $stmt=$mysqli->prepare($query);
         $stmt->bind_param('si',$this->name,$this->idsupercategory);
-        $stmt->execute();
-        if(!$stmt){
-            echo "preparation failed ".$mysqli->errno." : ".$mysqli->error."<br>";
-            exit();
-        }
-        if($stmt->affected_rows>0){
-            echo "Category was successfully inserted";
-        }
-        else{
-            echo "Category insertion failed";
-            exit();
-        }
+        return $stmt->execute();
         $stmt->close();
         $mysqli->close();
     }
@@ -64,7 +53,7 @@ class category{
     }
 
     // (b) Select By supercategory id (idsupercategory):
-    function selectsupcatbyid($id){
+    function selectcatbysupid($id){
         require 'config.php';
         $query="select * from category where idsupercategory=? and isdeleted != 1";
         $stmt=$mysqli->prepare($query);
@@ -114,6 +103,31 @@ class category{
         $mysqli->close();
     }
 
+    // (d) Select Super Categories
+    function selectsup(){
+        require 'config.php';
+        $query="select * from category where idcategory in(select idsupercategory from category where idsupercategory is NOT NULL) and isdeleted!=1";
+        $stmt=$mysqli->prepare($query);
+        $stmt->execute();
+        if(!$stmt){
+            echo "preparation failed ".$mysqli->errno." : ".$mysqli->error."<br>";
+            exit();
+        }
+        $result = $stmt->get_result();
+        $categories=[];
+        if($result) {
+            while($obj = $result->fetch_object()) {
+                array_push($categories, $obj);
+            }
+            return $categories;
+        }
+        else {
+            return false;
+        }
+        $stmt->close();
+        $mysqli->close();
+    }
+
     // 3. Update (UPDATE):
     function update(){
         require 'config.php';
@@ -123,18 +137,7 @@ class category{
         $name=$this->name;
         $idsupercategory=$this->idsupercategory;
         $stmt->bind_param('sii',$name,$idsupercategory,$idcategory);
-        $stmt->execute();
-        if(!$stmt){
-            echo "preparation failed ".$mysqli->errno." : ".$mysqli->error."<br>";
-            exit();
-        }
-        if($stmt->affected_rows>0){
-            echo "Category was successfully updated";
-        }
-        else{
-            echo "Category update failed";
-            exit();
-        }
+        return $stmt->execute();
         $stmt->close();
         $mysqli->close();
     }
