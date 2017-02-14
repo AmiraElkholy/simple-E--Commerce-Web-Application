df2408e906e6d47d2acc112305f3aee460b7799d
Sherifabdulmawla <sherifabdulmawla@gmail.com>
2017-02-14 07:15:57 +0200

    Adding Select independent Categories (Categories with no supercategory)

--- classes.php/category.php
+++ classes.php/category.php
@@ -20,6 +20,7 @@ class category{
         $query="insert into category(name,idsupercategory) values(?,?)";
         $stmt=$mysqli->prepare($query);
         $stmt->bind_param('si',$this->name,$this->idsupercategory);
+        
         return $stmt->execute();
         $stmt->close();
         $mysqli->close();
@@ -128,6 +129,31 @@ class category{
         $mysqli->close();
     }
 
+    // (e) Select independent Categories (Categories with no supercategory)
+    function selectind(){
+        require 'config.php';
+        $query="select * from category where idsupercategory IS NULL and isdeleted!=1";
+        $stmt=$mysqli->prepare($query);
+        $stmt->execute();
+        if(!$stmt){
+            echo "preparation failed ".$mysqli->errno." : ".$mysqli->error."<br>";
+            exit();
+        }
+        $result = $stmt->get_result();
+        $categories=[];
+        if($result) {
+            while($obj = $result->fetch_object()) {
+                array_push($categories, $obj);
+            }
+            return $categories;
+        }
+        else {
+            return false;
+        }
+        $stmt->close();
+        $mysqli->close();
+    }
+
     // 3. Update (UPDATE):
     function update(){
         require 'config.php';
@@ -137,7 +163,18 @@ class category{
         $name=$this->name;
         $idsupercategory=$this->idsupercategory;
         $stmt->bind_param('sii',$name,$idsupercategory,$idcategory);
-        return $stmt->execute();
+        $stmt->execute();
+        if(!$stmt){
+            echo "preparation failed ".$mysqli->errno." : ".$mysqli->error."<br>";
+            exit();
+        }
+        if($stmt->affected_rows>0){
+            echo "Category was successfully updated";
+        }
+        else{
+            echo "Category update failed";
+            exit();
+        }
         $stmt->close();
         $mysqli->close();
     }
