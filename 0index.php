a4b2a9c6d96f58d7f2f820248d9bf3259749302b
AmiraElkholy <AmiraElkholy@users.noreply.github.com>
2017-02-16 13:53:55 +0200

    added product.php page

--- 0index.php
+++ 0index.php
@@ -50,26 +50,7 @@
                 <div id="products">
                 <?php if($featuredproducts): ?>
                 <?php foreach ($featuredproducts as $product): ?>
-                    <div class="product">
-                        <a href="view-product.php?name=<?= $product->name ?>"><img id="productImage" src="img/products/<?= $product->image; ?>" alt="<?= $product->name ?>" class="feature"></a>
-
-                        <h3><a href="view-product.php?name=<?= $product->name ?>"><?= $product->name ?></a></h3>
-
-                        <p><?= $product->description; ?></p>
-
-
-                        <?php if($product->quantity > 0): ?>
-                        <h5>Availability: <span class="instock">In Stock</span></h5>
-                        <?php else: ?>
-                        <h5>Availability: <span class="outofstock">Out of Stock</span></h5>                            
-                        <?php endif; ?>
-
-                        <p>
-                            <a href="#" class="cart-btn">
-                                <span class="price">$<?= $product->price; ?></span>
-                                 <img src="img/white-cart.gif" alt="Add to Cart">ADD TO CART</a>
-                        </p>
-                    </div>
+                    <?php require 'product.php'; ?>
                 <?php endforeach; ?>
                 <?php endif; ?>
                 </div><!-- end products -->
--- category.php
+++ category.php
@@ -71,31 +71,11 @@
 
                 <?php if($products): ?>
                 <?php foreach ($products as $product): ?>
-                    <div class="product">
-                        <a href="view-product.php?name=<?= $product->name ?>"><img id="productImage" src="img/products/<?= $product->image; ?>" alt="<?= $product->name ?>" class="feature"></a>
-
-                        <h3><a href="view-product.php?name=<?= $product->name ?>"><?= $product->name ?></a></h3>
-
-                        <p><?= $product->description; ?></p>
-
-
-                        <?php if($product->quantity > 0): ?>
-                        <h5>Availability: <span class="instock">In Stock</span></h5>
-                        <?php else: ?>
-                        <h5>Availability: <span class="outofstock">Out of Stock</span></h5>                            
-                        <?php endif; ?>
-
-                        <p>
-                            <a href="#" class="cart-btn">
-                                <span class="price">$<?= $product->price; ?></span>
-                                 <img src="img/white-cart.gif" alt="Add to Cart">ADD TO CART</a>
-                        </p>
-                    </div>
+                    <?php require 'product.php'; ?>
                 <?php endforeach; ?>
                 <?php else: ?>
                     <?php if($subcategroies): ?>                      
                     <?php foreach ($subcategroies as $subcategory) {
-
                         $catproducts = product::selectAllbycatid($subcategory->idcategory); 
                         ?>
                         <?php if($catproducts): ?>
